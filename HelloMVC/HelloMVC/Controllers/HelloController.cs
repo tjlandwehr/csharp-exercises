019511816a650575989dba786f28397f9ad24591
Timothy Landwehr <tjlandwehr@gmail.com>
2018-03-03 09:51:42 -0600

    Finished with ASP.NET Core MVC Controller video

---
+++ HelloMVC/HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Content("<h1>Hello, World!</h1>", "text/html");
+        }
+
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye, World!");
+        }
+    }
+}
